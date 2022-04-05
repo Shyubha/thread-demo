7832f2652cea444028c69067b42bb7048b57a51f
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-05 09:56:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using System.Threading;
+
+namespace Thread_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadClass demo = new ThreadClass();
+            //Thread class we have created a thread > t1.
+            //ThreadStart is a delegate which hold method ref > that method will be exe by thread
+            Thread t1 = new Thread(new ThreadStart(demo.Test));
+            Thread t2 = new Thread(new ThreadStart(demo.Test));
+            Thread t3 = new Thread(new ThreadStart(demo.Test));
+            t1.Name = "First Thread";
+            t2.Name = "Second Thread";
+            t3.Name = "Third Thread";
+            //t1.Priority = ThreadPriority.Lowest;
+            //t2.Priority = ThreadPriority.Lowest;
+            //t3.Priority = ThreadPriority.Highest;
+            t1.Start();
+            t1.Join();//weit for exe t1 thread
+            Console.WriteLine("=========================");
+            t2.Start();
+            t2.Join();//weit for exe t2 thread
+            Console.WriteLine("=========================");
+            t3.Start();
+
+        }
+    }
+}
+
---
+++ Thread.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Thread_Example
+{
+    class TreadClass
+    {
+        public void Test()
+        {
+            lock (this) //this refers to the current thread
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(i);
+                    //Console.WriteLine(Thread.CurrentThread.Name);
+                    Thread.Sleep(700);
+                }
+            }
+        }
+        //public void Test1()
+        //{
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        Console.WriteLine("Hello");
+        //    }
+        //}
+    }
+}
